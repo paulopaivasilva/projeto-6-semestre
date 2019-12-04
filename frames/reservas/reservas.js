66bcc3e6e5b789851e1edc235a502866ca98c669
Paulo Henrique <paulohenriquepaiva@ymail.com>
2019-12-04 03:49:39 -0300

    Alterações

--- frames/reservas/reservas.js
+++ frames/reservas/reservas.js
@@ -1,4 +1,5 @@
 let url = 'frames/reservas/reservas.php'
+let id_hospede = ''
 
 function changeReservas(val) {
     if(val == 0){
@@ -30,6 +31,29 @@ function buscaHospede(){
     let dados = 'cpf=' + $('#cpfReserva').val()
     $.ajax({
         method: 'POST',
+        url: url,
+        data: dados,
+        success: (res) => {
+            let json = JSON.parse(res)
+            $('#nomeHospede').text(json.nome)
+            id_hospede = json.id
+            changeReservas(2)
+        }
+    })
+}
+
+function novaReserva(){
+    let dados = {
+        id_hospede: id_hospede,
+        checkin: $('#checkin').val(),
+        qtdHosp: $('#qtdHosp').val(),
+        quarto: $('#quarto').val(),
+        tpQuarto: $('#tpQuarto').val(),
+        checkout: $('#checkout').val()
+    }
+
+    $.ajax({
+        method: "POST",
         url: url,
         data: dados,
         success: (res) => {
--- frames/reservas/reservas.php
+++ frames/reservas/reservas.php
@@ -4,18 +4,32 @@
     if(isset($_POST['cpf'])){
         $cpf = $_POST['cpf'];
         $array = array();
-        $sql = "SELECT id_hosp,nome,cpf from hospedes where cpf like '%$cpf%'";
+        $sql = "SELECT id_hosp,nome from hospedes where cpf = $cpf";
         $resultado = mysqli_query($connect, $sql);
         if(mysqli_num_rows($resultado) > 0){
             $res = mysqli_fetch_array($resultado);
-            while ($row = mysqli_fetch_assoc($res)) {
-                $array1['id'] = $row['nome'];
-                $array1['col1'] = $row['cpf'];
-            
-                array_push($array,$array1);
-            }
-            echo json_encode($res);
+            $json = new stdClass();
+            $json->id = $res['id_hosp'];
+            $json->nome = $res['nome'];
+            $json ->cpf = $cpf;
+
+            echo json_encode($json);
         }
+    }
+
+    if(isset($_POST['id_hospede'])){
+        $id_hospede = $_POST['id_hospede'];
+        $checkin = isset($_POST['checkin']) ? $_POST['checkin'] : null;
+        $qtdHosp = $_POST['qtdHosp'];
+        $quarto = $_POST['quarto'];
+        $tpQuarto = $_POST['tpQuarto'];
+        $checkout = isset($_POST['checkout']) ? $_POST['checkout'] : null;
+
+        $sql = "INSERT INTO reservas (id_hospede, dt_reserva, dt_checkin, dt_checkout, id_quarto, id_usuario, status_reserva)
+        values($id_hospede,now(),$checkin,$checkout,$quarto,$id_hospede,1)";
+        echo $sql;
+        $resultado = mysqli_query($connect, $sql);
+        echo "Reserva efetuada!";
     }
 
 ?>
