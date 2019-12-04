93fe0034d3bde3893fcd0a8b54ef3feb67a2a6ab
Paulo Henrique <paulohenriquepaiva@ymail.com>
2019-12-04 16:36:09 -0300

    Alterações

--- frames/reservas/reservas.js
+++ frames/reservas/reservas.js
@@ -1,12 +1,18 @@
 let url = 'frames/reservas/reservas.php'
 let id_hospede = ''
+let id_reserva = ''
 
-function changeReservas(val) {
+function changeReservas(val, res) {
     if(val == 0){
         $('#nivelReserva1').hide()
         $('#nivelReserva2').hide()
         $('#nivelReserva3').hide()
         $('#nivelReserva4').hide()
+        $('#nivelReserva5').hide()
+        $('#nivelReserva6').hide()
+        $('#nivelReserva7').hide()
+        $('#nivelReserva8').hide()
+        $('#nivelReserva9').hide()
         $('#nivelReserva0').show()
     }
     if(val == 1){
@@ -22,8 +28,38 @@ function changeReservas(val) {
         $('#nivelReserva3').show()
     }
     if(val == 4){
+        $('#statusReserva').text(res)
         $('#nivelReserva4').show()
         $('#nivelReserva3').hide()
+    }
+    if(val == 5){
+        $('#nivelReserva0').hide()
+        $('#nivelReserva1').hide()
+        $('#nivelReserva2').hide()
+        $('#nivelReserva3').hide()
+        $('#nivelReserva4').hide()
+        $('#nivelReserva5').show()
+    }
+    if(val == 6){
+        $('#nivelReserva5').hide()
+        $('#nivelReserva6').show()
+    }
+    if(val == 7){
+        $('#nivelReserva0').hide()
+        $('#nivelReserva1').hide()
+        $('#nivelReserva2').hide()
+        $('#nivelReserva3').hide()
+        $('#nivelReserva4').hide()
+        $('#nivelReserva5').hide()
+        $('#nivelReserva7').show()
+    }
+    if(val == 8){
+        $('#nivelReserva7').hide()
+        $('#nivelReserva8').show()
+    }
+    if(val == 9){
+        $('#nivelReserva8').hide()
+        $('#nivelReserva9').show()
     }
 }
 
@@ -42,14 +78,32 @@ function buscaHospede(){
     })
 }
 
+function consultarQuarto(){
+    $.ajax({
+        method: 'POST',
+        url: url,
+        data: { consultaQuarto: true, tipoQuarto: $('#tpQuarto').val() },
+        success: (res) => {
+            if(res != 0)
+                $('#quarto').val(res)
+            else
+                $('#quarto').val('Nenhum quarto disponível')
+        }
+    })
+}
+
 function novaReserva(){
+    let checkin = $('#checkin').val()
+    let checkout = $('#checkout').val()
+    if(checkin == "") checkin = null
+    if(checkout == "") checkout = null
     let dados = {
         id_hospede: id_hospede,
-        checkin: $('#checkin').val(),
+        checkin: checkin,
         qtdHosp: $('#qtdHosp').val(),
         quarto: $('#quarto').val(),
         tpQuarto: $('#tpQuarto').val(),
-        checkout: $('#checkout').val()
+        checkout: checkout
     }
 
     $.ajax({
@@ -57,7 +111,65 @@ function novaReserva(){
         url: url,
         data: dados,
         success: (res) => {
-            console.log(res)
+            $('#tpQuarto').val('')
+            $('#qtdHosp').val('')
+            $('#quarto').val('')
+            changeReservas(4, res)
+        }
+    })
+}
+
+function consultaReserva(val){
+    let reserva
+    if($('#reservaBusca').val() != '' || $('#reservaCancela').val() != ''){
+        if(val == 8)
+            reserva = $('#reservaCancela').val()
+        else
+            reserva = $('#reservaBusca').val()
+    }
+
+    $.ajax({
+        method: 'POST',
+        url: url,
+        data: { buscaReserva: reserva},
+        success: (res) => {
+            res = JSON.parse(res)
+            if(val != 8){
+                $('#nomeHospede').text(res.nome)
+                $('#consultaReserva').text(res.id_reserva)
+                $('#consultaHospede').text(res.nome)
+                $('#consultaCPF').text(res.cpf)
+                $('#consultaCheckin').text(res.dt_checkin)
+                $('#consultaCheckout').text(res.dt_checkout)
+                $('#consultaTpQuarto').text(res.DescTipoquarto)
+                $('#consultaQuarto').text(res.numero)
+                $('#reservaBusca').val('')
+            }else{
+                $('#cancelaReserva').text(res.id_reserva)
+                $('#cancelaHospede').text(res.nome)
+                $('#cancelaCPF').text(res.cpf)
+                $('#cancelaCheckin').text(res.dt_checkin)
+                $('#cancelaCheckout').text(res.dt_checkout)
+                $('#cancelaTpQuarto').text(res.DescTipoquarto)
+                $('#cancelaQuarto').text(res.numero)
+                $('#reservaCancela').val('')
+                id_reserva = res.id_reserva
+            }
+            changeReservas(val)
+        }
+    })
+}
+
+function cancelarReserva(){
+    $.ajax({
+        method: 'POST',
+        url: url,
+        data: { idCancela: id_reserva },
+        success: (res) => {
+            if(res){
+                $('#statusCancela').text(res)
+                changeReservas(9)
+            }
         }
     })
 }
--- frames/reservas/reservas.php
+++ frames/reservas/reservas.php
@@ -19,17 +19,70 @@
 
     if(isset($_POST['id_hospede'])){
         $id_hospede = $_POST['id_hospede'];
-        $checkin = isset($_POST['checkin']) ? $_POST['checkin'] : null;
+        $checkin = $_POST['checkin'];
         $qtdHosp = $_POST['qtdHosp'];
         $quarto = $_POST['quarto'];
         $tpQuarto = $_POST['tpQuarto'];
-        $checkout = isset($_POST['checkout']) ? $_POST['checkout'] : null;
+        $checkout = $_POST['checkout'];
+        $id_usuario = 1000;
 
         $sql = "INSERT INTO reservas (id_hospede, dt_reserva, dt_checkin, dt_checkout, id_quarto, id_usuario, status_reserva)
-        values($id_hospede,now(),$checkin,$checkout,$quarto,$id_hospede,1)";
-        echo $sql;
+        values($id_hospede,now(),null,null,$quarto,$id_usuario,1)";
+
+        $sql2 = "UPDATE `progr740_hotel`.`quartos` SET `status_quarto` = 'Ocupado' WHERE (`id_quarto` = '$quarto')";
         $resultado = mysqli_query($connect, $sql);
-        echo "Reserva efetuada!";
+        $resultado2 = mysqli_query($connect, $sql2);
+        echo "Reserva confirmada!";
     }
 
+    if(isset($_POST['consultaQuarto'])){
+        $tipo = $_POST['tipoQuarto'];
+        $sql = "select id_quarto from quartos WHERE status_quarto = 'Disponivel' AND IDTipoQuarto = $tipo LIMIT 1";
+        $resultado = mysqli_query($connect, $sql);
+        if(mysqli_num_rows($resultado) > 0){
+            $res = mysqli_fetch_array($resultado);
+            echo $res['id_quarto'];
+        }else{
+            echo 0;
+        }
+    }
+
+    if(isset($_POST['buscaReserva'])){
+        $reserva = $_POST['buscaReserva'];
+        $sql = "SELECT r.id_reserva,r.id_hospede,r.dt_reserva,r.dt_checkin,r.dt_checkout,h.nome
+                ,Case h.cpf when null then 'RG' else 'CPF' end as TipoDocumento
+                ,h.cpf
+                ,q.numero
+                ,tq.DescTipoquarto
+                ,sr.DescStatusReserva
+        from reservas r
+        inner join hospedes h on r.id_hospede = h.id_hosp
+        inner join quartos q on q.id_quarto = r.id_quarto
+        inner join TipoQuarto tq on tq.idTipoQuarto = q.IDTipoQuarto
+        inner join StatusReserva sr on r.status_reserva = sr.id
+        where r.id_reserva = $reserva
+        ";
+        $resultado = mysqli_query($connect, $sql);
+        if(mysqli_num_rows($resultado) > 0){
+            $res = mysqli_fetch_array($resultado);
+            $json = new stdClass();
+            $json->id_reserva = $res['id_reserva'];
+            $json->nome = $res['nome'];
+            $json->dt_checkin = $res['dt_checkin'];
+            $json ->dt_checkout = $res['dt_checkout'];
+            $json->cpf = $res['cpf'];
+            $json->DescTipoquarto = $res['DescTipoquarto'];
+            $json ->numero = $res['numero'];
+            echo json_encode($json);
+        }else{
+            echo 0;
+        }
+    }
+
+    if(isset($_POST['idCancela'])){
+        $id_reserva = $_POST['idCancela'];
+        $sql = "UPDATE reservas set status_reserva = 2 WHERE id_reserva = $id_reserva";
+        $resultado = mysqli_query($connect, $sql);
+        echo "Reserva Cancelada!";
+    }
 ?>
